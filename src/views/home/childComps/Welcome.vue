c1bf1fa08b0bb3f83055a69d2138224978e50b0d
cuteSheep <2451228780@qq.com>
2021-09-24 09:28:54 +0800

    welcome

--- src/views/home/childComps/Welcome.vue
+++ src/views/home/childComps/Welcome.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="welcome">
+    <span class="title">欢迎光临~</span>
   </div>
 </template>
 
@@ -11,9 +12,19 @@ export default {
 
 <style lang="less" scoped>
   #welcome {
-    background: url("~assets/img/login/bg6.jpg") no-repeat;
+    position: relative;
+    background: url("~assets/img/login/bg9.jpg") no-repeat;
     background-size:100% 100%;
     width: 100%;
     height: 100%;
+    color: rgb(44, 40, 40);
+    .title {
+      font-size: 100px;
+      line-height: 100px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -80%);
+    }
   }
 </style>
